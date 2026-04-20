7a165f58b4e72bdd99eca7fa5d149891f209ab3b
Alexey Panfilov <alexey.panfilov@constructor.tech>
2026-04-20 14:55:16 +0200

    Initial Commit

---
+++ health-syncTests/health_syncTests.swift
@@ -0,0 +1,19 @@
+//
+//  health_syncTests.swift
+//  health-syncTests
+//
+//  Created by Alexey Panfilov on 20. 4. 2026..
+//
+
+import Testing
+@testable import health_sync
+
+struct health_syncTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
